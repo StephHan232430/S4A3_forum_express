946fceab42c7ba1079b7b5dd5b559f651ea7b984
Steph <blackman914037@gmail.com>
2020-02-18 10:51:57 +0800

    Add pagination on restaurants index

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,22 +1,39 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const pageLimit = 10
 
 const restController = {
   getRestaurants: (req, res) => {
+    let offset = 0 // 偏移幾筆後“開始”算
     let whereQuery = {}
     let categoryId = ''
+
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
+
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId)
       whereQuery['CategoryId'] = categoryId
     }
-    Restaurant.findAll({
+
+    Restaurant.findAndCountAll({
       include: Category,
       where: whereQuery,
       nest: true,
-      raw: true
-    }).then(restaurants => {
-      const data = restaurants.map(r => ({
+      raw: true,
+      offset,
+      limit: pageLimit
+    }).then(result => {
+      let page = Number(req.query.page) || 1
+      let pages = Math.ceil(result.count / pageLimit)
+      let totalPage = Array.from({ length: pages }).map(
+        (item, index) => index + 1 // array是給handlebars的each-helper用的，產生分頁按鈕的數字
+      )
+      let prev = page - 1 < 1 ? 1 : page - 1
+      let next = page + 1 > pages ? pages : page + 1
+      const data = result.rows.map(r => ({
         ...r,
         description: r.description.substring(0, 50)
       }))
@@ -24,7 +41,11 @@ const restController = {
         return res.render('restaurants', {
           restaurants: data,
           categories,
-          categoryId
+          categoryId,
+          page,
+          totalPage,
+          prev,
+          next
         })
       })
     })
