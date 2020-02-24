d82dacc9ff8b80fc03e5506efa685ef3d16a5c65
Steph <blackman914037@gmail.com>
2020-02-24 21:16:07 +0800

    PUT /api/admin/categories/:id

--- routes/apis.js
+++ routes/apis.js
@@ -28,6 +28,8 @@ router.get('/admin/categories', categoryController.getCategories)
 
 router.post('/admin/categories', categoryController.postCategory)
 
+router.put('/admin/categories/:id', categoryController.putCategory)
+
 router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 
 module.exports = router
